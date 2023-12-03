4c895705ba41380af97bd866a6a6fa80fef38ab9
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2023-12-03 12:06:24 +0900

    Add new column types and default values

--- columnBuilder.js
+++ columnBuilder.js
@@ -15,6 +15,56 @@ class ColumnBuilder {
         return this;
     }
 
+    smallint() {
+        this.column += 'SMALLINT';
+        return this;
+    }
+
+    autoInteger() {
+        this.column += 'INTEGER AUTO_INCREMENT';
+        return this;
+    }
+
+    bigint() {
+        this.column += 'BIGINT';
+        return this;
+    }
+
+    tinyint() {
+        this.column += 'TINYINT';
+        return this;
+    }
+
+    boolean() {
+        this.column += 'BOOLEAN';
+        return this;
+    }
+
+    timestamp() {
+        this.column += 'TIMESTAMP';
+        return this;
+    }
+
+    autoTimestamp() {
+        this.column += 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP';
+        return this;
+    }
+
+    date() {
+        this.column += 'DATE';
+        return this;
+    }
+
+    datetime() {
+        this.column += 'DATETIME';
+        return this;
+    }
+
+    default(value) {
+        this.column += ` DEFAULT ${value}`;
+        return this;
+    }
+
     text() {
         this.column += 'TEXT';
         return this;
@@ -22,6 +72,76 @@ class ColumnBuilder {
 
     varchar(length) {
         this.column += `VARCHAR(${length})`;
+        return this;
+    }
+
+    varbinary(length) {
+        this.column += `VARBINARY(${length})`;
+        return this;
+    }
+
+    blob() {
+        this.column += 'BLOB';
+        return this;
+    }
+
+    binary(length) {
+        this.column += `BINARY(${length})`;
+        return this;
+    }
+
+    decimal(length, decimal) {
+        this.column += `DECIMAL(${length},${decimal})`;
+        return this;
+    }
+
+    float(length, decimal) {
+        this.column += `FLOAT(${length},${decimal})`;
+        return this;
+    }
+
+    double(length, decimal) {
+        this.column += `DOUBLE(${length},${decimal})`;
+        return this;
+    }
+
+    abstract() {
+        this.column += 'ABSTRACT';
+        return this;
+    }
+
+    array() {
+        this.column += 'ARRAY';
+        return this;
+    }
+
+    enum(...values) {
+        this.column += `ENUM(${values.join(',')})`;
+        return this;
+    }
+
+    json() {
+        this.column += 'JSON';
+        return this;
+    }
+
+    jsonb() {
+        this.column += 'JSONB';
+        return this;
+    }
+
+    char(length) {
+        this.column += `CHAR(${length})`;
+        return this;
+    }
+
+    currency(length, decimal) {
+        this.column += `CURRENCY(${length},${decimal})`;
+        return this;
+    }
+
+    current_timestamp() {
+        this.column += 'CURRENT_TIMESTAMP';
         return this;
     }
 
